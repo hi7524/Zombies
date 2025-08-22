f86a19f8825be134330d44aec16d9ea6fbf581fd
김혜인 <rjqnrdl7524@naver.com>
2025-08-22 16:29:39 +0900

    Player 이동 및 회전

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+//public static class TagManager
+//{
+//    public static readonly string Player = "Player";
+//}
+
+public class PlayerMovement : MonoBehaviour
+{
+    private static readonly int MoveHash = Animator.StringToHash("Move");
+
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 180f;
+
+    private PlayerInput playerInput;
+    private Rigidbody rb;
+    private Animator animator;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()
+    {
+        // 회전
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(0f, playerInput.Roatate * rotationSpeed * Time.fixedDeltaTime, 0f));
+
+        // 이동
+        rb.MovePosition(rb.position + transform.forward * playerInput.Move * moveSpeed * Time.fixedDeltaTime);
+
+        
+        animator.SetFloat(MoveHash, playerInput.Move);
+    }
+}
